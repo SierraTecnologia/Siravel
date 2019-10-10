49526beb6387daa677c6786969c25a9146c50adb
Ricardo Rebello Sierra <contato@ricardosierra.com.br>
2019-10-10 04:50:42 -0300

    Update Routes

--- siravel/Routes/web.php
+++ siravel/Routes/web.php
@@ -2,15 +2,9 @@
 
 //Route::group(['middleware' => ['siravel-analytics']], function () {                                                                                                                           
     $alias = 'public.';
-    
-    Route::get('/',         ['as' => $alias . 'home',   'uses' => 'PagesController@getHome']);
-    Route::get('contato', array('as' => $alias . 'contact', 'uses' =>'PagesController@contact'));
-    Route::post('/contact', ['as' => $alias . 'contact',   'uses' => 'PagesController@postContact']);
-
-    Auth::routes();
-
     include dirname(__FILE__) . DIRECTORY_SEPARATOR . "web". DIRECTORY_SEPARATOR . "admin.php";
     include dirname(__FILE__) . DIRECTORY_SEPARATOR . "web". DIRECTORY_SEPARATOR . "components.php";
+    include dirname(__FILE__) . DIRECTORY_SEPARATOR . "web". DIRECTORY_SEPARATOR . "plugins.php";
     // include dirname(__FILE__) . DIRECTORY_SEPARATOR . "web". DIRECTORY_SEPARATOR . "wiki.php";
-    include dirname(__FILE__) . DIRECTORY_SEPARATOR . "web". DIRECTORY_SEPARATOR . "book.php";                                                                              
+    // include dirname(__FILE__) . DIRECTORY_SEPARATOR . "web". DIRECTORY_SEPARATOR . "book.php";
 //});    
