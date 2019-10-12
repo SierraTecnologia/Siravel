d77d090003ec9f1b456d76ae585f2cf3eb716b88
Ricardo Rebello Sierra <contato@ricardosierra.com.br>
2019-10-11 22:57:04 -0300

    Novas Rotas

---
+++ siravel/Http/Controllers/AdminController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Siravel\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use App\Models\Banner;
+use App\Models\Link;
+use App\Models\ActiveUser;
+use App\Models\HotTopic;
+use App\Models\Image;
+use Illuminate\Http\Request;
+use Auth;
+use Siravel\Models\Identity\Person;
+
+class AdminController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($modulo, Request $request)
+    {
+        $modulos = [
+            'persons' => Person::class,
+        ];
+
+        $loadClass = $modulos[$modulo];
+
+        return (new \SierraTecnologia\Facilitador\Controllers\RepositoryController(
+            new \SierraTecnologia\Facilitador\Services\RepositoryService($loadClass)
+        ))->index($request);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($modulo, Request $request)
+    {
+        $modulos = [
+            'persons' => Person::class,
+        ];
+
+        $loadClass = $modulos[$modulo];
+
+        return (new \SierraTecnologia\Facilitador\Controllers\RepositoryController(
+            new \SierraTecnologia\Facilitador\Services\RepositoryService($loadClass)
+        ))->index($request);
+    }
+
+}
--- siravel/Routes/web/admin.php
+++ siravel/Routes/web/admin.php
@@ -1,28 +1,33 @@
 <?php
 
 Route::prefix('admin')->group(function () {
-    Route::namespace('Admin')->group(function () {
-        Route::get('/', 'HomeController@index');
-        Route::get('/home', 'HomeController@index')->name('home');
-        Route::resource('users', 'UserController');
+    // ADmin Router
+    Route::get('/gerenciar/{modulo}', 'AdminController@index');
+    Route::get('/gerenciar/{modulo}/{id}', 'AdminController@show');
+    Route::post('/gerenciar/{modulo}/', 'AdminController@store');
+    Route::get('/gerenciar/{modulo}/{id}/edit', 'AdminController@edit');
+    Route::put('/gerenciar/{modulo}/{id}', 'AdminController@update');
+    Route::delete('/gerenciar/{modulo}/{id}', 'AdminController@destroy');
 
-        Route::resource('languages', 'LanguageController');
-        Route::resource('sectors', 'SectorController');
-        Route::resource('services', 'ServiceController');
-        Route::resource('ambientes', 'AmbienteController');
 
-        Route::resource('actions', 'ActionController');
-        Route::get('actions/model/{model}', 'ActionController@actionsForModel')->name('actions.model');
-        Route::get('actions/execute/{modelId}/{actionCod}', 'ActionController@executeAction')->name('actions.execute');
-        
-        Route::resource('databases', 'DatabaseController');
-        Route::resource('databaseCollections', 'DatabaseCollectionsController');
-        Route::resource('projects', 'ProjectController');
-        Route::resource('collaborators', 'CollaboratorController');
-        Route::resource('runners', 'RunnerController');
-        Route::resource('loggers', 'LoggerController');
-        Route::resource('computers', 'ComputerController');
-        Route::resource('domains', 'DomainController');
-        Route::resource('settings', 'SettingController');
-    });
+    // Route::resource('users', 'UserController');
+
+    // Route::resource('languages', 'LanguageController');
+    // Route::resource('sectors', 'SectorController');
+    // Route::resource('services', 'ServiceController');
+    // Route::resource('ambientes', 'AmbienteController');
+
+    // Route::resource('actions', 'ActionController');
+    // Route::get('actions/model/{model}', 'ActionController@actionsForModel')->name('actions.model');
+    // Route::get('actions/execute/{modelId}/{actionCod}', 'ActionController@executeAction')->name('actions.execute');
+    
+    // Route::resource('databases', 'DatabaseController');
+    // Route::resource('databaseCollections', 'DatabaseCollectionsController');
+    // Route::resource('projects', 'ProjectController');
+    // Route::resource('collaborators', 'CollaboratorController');
+    // Route::resource('runners', 'RunnerController');
+    // Route::resource('loggers', 'LoggerController');
+    // Route::resource('computers', 'ComputerController');
+    // Route::resource('domains', 'DomainController');
+    // Route::resource('settings', 'SettingController');
 });
