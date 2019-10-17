2c02998e14d21c5ef040c8996a465bc5686ddb12
Ricardo Rebello Sierra <contato@ricardosierra.com.br>
2019-10-17 06:42:23 -0300

    Update Gravatar Service

--- siravel/Providers/GravatarServiceProvider.php
+++ siravel/Providers/GravatarServiceProvider.php
@@ -4,8 +4,26 @@ namespace Siravel\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class GravatarServiceProvider extends ServiceProvider {
+class GravatarServiceProvider extends ServiceProvider
+{
 
+    /**
+     * Boot the service provider.
+     */
+    public function boot()
+    {
+        $this->setupConfig();
+    }
+    /**
+     * Setup the config.
+     */
+    protected function setupConfig()
+    {
+        $source = realpath(__DIR__.'/../Publishes/config/gravatar.php');
+        $this->publishes([$source => config_path('gravatar.php')]);
+        $this->mergeConfigFrom($source, 'gravatar');
+	}
+	
 	/**
 	 * Register the service provider.
 	 *
@@ -14,6 +32,13 @@ class GravatarServiceProvider extends ServiceProvider {
 	public function register() {
 
 		$this->app->bind('GravatarService', 'Siravel\Services\GravatarService');
+
+		/**
+		 * Adicionei separado do de cima
+		 */
+        $this->app->singleton('gravatar', function ($app) {
+            return new Gravatar($this->app['config']);
+        });
 
 	}
 
--- siravel/Services/Gravatar.php
+++ siravel/Services/Gravatar.php
@@ -1,68 +1,127 @@
 <?php
+/**
+ * Analisando Dependncia
+ */
 
 namespace Siravel\Services;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
+use Illuminate\Contracts\Config\Repository as Config;
 
-class Gravatar {
+class Gravatar extends GravatarService
+{
+    /**
+     * The maximum size allowed for the Gravatar.
+     */
+    const MAX_SIZE = 512;
 
-	const BASE_HTTP  = 'http://www.gravatar.com/avatar/';
-	const BASE_HTTPS = 'https://secure.gravatar.com/avatar/';
+    /**
+     * The default size of the Gravatar.
+     *
+     * @var int
+     */
+    private $defaultSize = null;
 
-	/**
-	 * Guzzle HTTP Client.
-	 *
-	 * @var \GuzzleHttp\Client
-	 */
-	private $client;
+    public function __construct(Config $config)
+    {
+        // Set default configuration values
+        $this->setDefaultImage($config->get('gravatar.default'));
+        $this->defaultSize = $config->get('gravatar.size');
+        $this->setMaxRating($config->get('gravatar.maxRating', 'g'));
 
-	/**
-	 * Create new Gravatar helper instance.
-	 *
-	 * @param \GuzzleHttp\Client $client
-	 */
-	public function __construct(Client $client) {
-		$this->client = $client;
-	}
+        $this->enableSecureImages();
+    }
 
-	/**
-	 * Build a gravatar url.
-	 *
-	 * @param        $email
-	 * @param int    $size
-	 * @param string $default
-	 * @param bool   $https
-	 *
-	 * @return string
-	 */
-	public function buildURL($email, $size = 100, $default = 'mm', $https = true) {
-		$url = ($https) ? self::BASE_HTTPS : self::BASE_HTTP;
+    /**
+     * Return the URL of a Gravatar. Note: it does not check for the existence of this Gravatar.
+     *
+     * @param string      $email  The email address.
+     * @param int         $size   Override the size of the Gravatar.
+     * @param null|string $rating Override the default rating if you want to.
+     *
+     * @return string The URL of the Gravatar.
+     */
+    public function src($email, $size = null, $rating = null)
+    {
+        if (is_null($size)) {
+            $size = $this->defaultSize;
+        }
 
-		$url .= md5($email);
-		$url .= '?s=' . $size;
-		$url .= '&d=' . $default;
+        $size = max(1, min(self::MAX_SIZE, $size));
 
-		return $url;
-	}
+        $this->setAvatarSize($size);
 
-	/**
-	 * Check if a gravatar exists.
-	 *
-	 * @param $email
-	 *
-	 * @return bool
-	 */
-	public function exists($email) {
-		$gravatar = $this->buildURL($email, 100, 404);
+        if (!is_null($rating)) {
+            $this->setMaxRating($rating);
+        }
 
-		try {
-			$this->client->head($gravatar);
+        return $this->buildGravatarURL($email);
+    }
 
-			return true;
-		} catch (RequestException $e) {
-			return false;
-		}
-	}
+    /**
+     * Return the code of HTML image for a Gravatar.
+     *
+     * @param string      $email      The email address.
+     * @param string      $alt        The alt attribute for the image.
+     * @param array       $attributes Override the 'height' and the 'width' of the image if you want.
+     * @param null|string $rating     Override the default rating if you want to.
+     *
+     * @return string The code of the HTML image.
+     */
+    public function image($email, $alt = null, $attributes = [], $rating = null)
+    {
+        $dimensions = [];
 
-} 
+        if (array_key_exists('width', $attributes)) {
+            $dimensions[] = $attributes['width'];
+        }
+        if (array_key_exists('height', $attributes)) {
+            $dimensions[] = $attributes['height'];
+        }
+
+        if (count($dimensions) > 0) {
+            $size = min(self::MAX_SIZE, max($dimensions));
+        } else {
+            $size = $this->defaultSize;
+        }
+
+        $src = $this->src($email, $size, $rating);
+
+        if (!array_key_exists('width', $attributes) && !array_key_exists('height', $attributes)) {
+            $attributes['width'] = $this->size;
+            $attributes['height'] = $this->size;
+        }
+
+        return $this->formatImage($src, $alt, $attributes);
+    }
+
+    /**
+     * Check if a Gravatar image exists.
+     *
+     * @param string $email The email address.
+     *
+     * @return bool True if the Gravatar exists, false otherwise.
+     */
+    public function exists($email)
+    {
+        $this->setDefaultImage('404');
+
+        $url = $this->buildGravatarURL($email);
+        $headers = get_headers($url, 1);
+
+        return substr($headers[0], 9, 3) == '200';
+    }
+
+    /**
+     * Get the HTML image code.
+     *
+     * @param string $src        The source attribute of the image.
+     * @param string $alt        The alt attribute of the image.
+     * @param array  $attributes Used to set the width and the height.
+     *
+     * @return string The HTML code.
+     */
+    private function formatImage($src, $alt, $attributes)
+    {
+        return sprintf('<img src="%s" alt="%s" height="%s" width="%s">', $src, $alt, $attributes['height'], $attributes['width']);
+    }
+}
