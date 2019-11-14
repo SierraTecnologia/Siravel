7371e04bca71705be1051cd198a2ecc713eed734
Ricardo Rebello Sierra <contato@ricardosierra.com.br>
2019-11-14 20:13:36 -0300

    FIx bugs

--- siravel/Models/Identity/Business/Business.php
+++ siravel/Models/Identity/Business/Business.php
@@ -14,27 +14,20 @@ class Business extends Model
     protected $primaryKey = 'code';
     protected $keyType = 'string';
 
-
-    protected $table = 'businesses';       
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        // 'user_id',
         'name',
-        'code'
+        'code',
+        'language_code',
+        'money_code',
+        'user_id'
     ];
 
-
     protected $mappingProperties = array(
-
-        'user_id' => [
-            'type' => 'integer',
-            "analyzer" => "standard",
-        ],
         'name' => [
             'type' => 'string',
             "analyzer" => "standard",
@@ -43,11 +36,145 @@ class Business extends Model
             'type' => 'string',
             "analyzer" => "standard",
         ],
+        'language_code' => [
+            'type' => 'string',
+            "analyzer" => "standard",
+        ],
+        'money_code' => [
+            'type' => 'string',
+            "analyzer" => "standard",
+        ],
+        'user_id' => [
+            'type' => 'string',
+            "analyzer" => "standard",
+        ],
     );
-
-    public function user()
+        
+    /**
+     * Get all of the owning businessable models.
+     */
+    public function businessable()
     {
-        return $this->belongsTo('App\Models\User', 'user_id', 'id');
+        return $this->morphTo(); //, 'businessable_type', 'businessable_code'
+    }
+
+    /**
+     * Get all of the users that are assigned this tag.
+     */
+    public function users()
+    {
+        return $this->morphedByMany('App\Models\User', 'businessable'); //, 'businessable_type', 'businessable_code');
+    }
+
+
+    // /**
+    //  * Get all of the features for the post.
+    //  */
+    // public function features()
+    // {
+    //     return $this->morphToMany('App\Models\Feature', 'featureable');
+    // }
+
+    // /**
+    //  * Get all of the plugins for the post.
+    //  */
+    // public function plugins()
+    // {
+    //     return $this->morphToMany('App\Models\Plugin', 'pluginable');
+    // }
+
+    // /**
+    //  * Get all of the widgets for the post.
+    //  */
+    // public function widgets()
+    // {
+    //     return $this->morphToMany('App\Models\Negocios\Widget', 'widgetable');
+    // }
+
+    // /**
+    //  * Get all of the settings for the post.
+    //  */
+    // public function settings()
+    // {
+    //     return $this->hasMany('App\Models\Setting');
+    // }
+
+    // /**
+    //  * Get all of the subscriptions for the post.
+    //  */
+    // public function subscriptions()
+    // {
+    //     return $this->hasMany('App\Models\Negocios\Subscription');
+    // }
+
+    /**
+     * Get all of the addresses for the post.
+     */
+    public function addresses()
+    {
+        return $this->morphToMany('Siravel\Models\Identity\Address', 'addresseable');
+    }
+
+    /**
+     * Get all of the phones for the post.
+     */
+    public function phones()
+    {
+        return $this->morphToMany('Siravel\Models\Identity\Phone', 'phoneable');
+    }
+
+    /**
+     * Get all of the post's accounts.
+     */
+    public function accounts()
+    {
+        return $this->morphMany('Siravel\Models\Identity\Account', 'accountable');
+    }
+
+    /**
+     * Retorna se é ou não o busines padrão
+     *
+     * @return boolean
+     */
+    public function isDefault()
+    {
+        return \Siravel\Services\System\BusinessService::getSingleton()->isDefault($this);
+    }
+
+
+
+    /**
+     * Outros Relatiosn Nad a aver
+     *
+     * @return void
+     */
+
+    public function sitios()
+    {
+        return $this->morphToMany('Siravel\Models\Identity\Hability\Sitio', 'sitioable');
+    }
+    /**
+     * Get all of the skills for the post.
+     */
+    public function skills()
+    {
+        return $this->morphToMany('Siravel\Models\Identity\Hability\Skill', 'skillable');
+    }
+    public function infos()
+    {
+        return $this->morphMany('Siravel\Models\Identity\Hability\Info', 'infoable');
+    }
+    public function gostos()
+    {
+        return $this->morphToMany('Siravel\Models\Identity\Hability\Gosto', 'gostoable');
+    }
+    public function pircings()
+    {
+        return $this->morphToMany('Siravel\Models\Identity\Hability\Pircing', 'pircingable');
+    }
+    public function tatuages()
+    {
+        return $this->morphToMany('Siravel\Models\Identity\Hability\Tatuagem', 'tatuageable');
     }
 
 }
