f316dbcf6412e36bdcd4d2cae0fcbf4f7e711d01
Ricardo Rebello Sierra <contato@ricardosierra.com.br>
2019-11-20 02:49:54 -0300

    Fix bug

--- siravel/Models/Market/Actors/Bank.php
+++ siravel/Models/Market/Actors/Bank.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Siravel\Models\Market\Actions;
+namespace Siravel\Models\Market\Actors;
 
 use Siravel\Models\Model;
 use SiObjects\Support\Traits\Models\ComplexRelationamentTrait;
