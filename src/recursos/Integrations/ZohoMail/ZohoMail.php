51080ac2d365bbdd18c887abc631f1de8cad7e3f
Ricardo Rebello Sierra <contato@ricardosierra.com.br>
2019-12-03 10:35:11 -0300

    Corrigindo Bugs

---
+++ src/recursos/Integrations/ZohoMail/ZohoMail.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SiWeapons\Integrations\ZohoMail;
+
+use Illuminate\Support\Facades\Log;
+// use Siravel\Models\Entytys\Digital\Midia\Video;
+use App\Models\User;
+use SiWeapons\Integrations\Integration;
+
+class ZohoMail extends Integration
+{
+    public static $ID = 25;
+    public static $URL = 'https://www.zohoMail.com/';
+
+    public function getConnection($organizer = false)
+    {
+        return false;
+    }
+}
