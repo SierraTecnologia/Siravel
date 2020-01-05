02271b98fa170e2059ab6ac02f6da4dc827f0cc8
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-01-04 21:48:34 -0300

    Corrigindo erros nos modelos

---
+++ siravel/Contants/Tables.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Siravel\Contants;
+
+/**
+ * Class Constant.
+ *
+ * @package Siravel\Contants
+ */
+final class Tables
+{
+    public const TABLE_COMMENTS = 'comments';
+    public const TABLE_POSTS_COMMENTS = 'post_comments';
+    public const TABLE_NOTIFICATIONS = 'notifications';
+    public const TABLE_POSTS_NOTIFICATIONS = 'post_notifications';
+
+    // Others
+    public const TABLE_LOCATIONS = 'locations';
+    public const TABLE_PHOTOS = 'photos';
+    public const TABLE_PHOTOS_THUMBNAILS = 'photos_thumbnails';
+    public const TABLE_POSTS = 'posts';
+    public const TABLE_POSTS_PHOTOS = 'posts_photos';
+    public const TABLE_POSTS_TAGS = 'posts_tags';
+    public const TABLE_ROLES = 'roles';
+    public const TABLE_SUBSCRIPTIONS = 'subscriptions';
+    public const TABLE_TAGS = 'tags';
+    public const TABLE_THUMBNAILS = 'thumbnails';
+    public const TABLE_USERS = 'users';
+}
