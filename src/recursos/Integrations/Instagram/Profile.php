b29b8fee03e957383e6a6cd4aeeef7f4f665a955
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-01-17 09:40:02 -0300

    Criando Trackiadores de Modelos no Finder

--- src/recursos/Integrations/Instagram/Profile.php
+++ src/recursos/Integrations/Instagram/Profile.php
@@ -13,28 +13,45 @@ class Profile extends Instagram
         
     }
 
-    public function getPosts()
+    public static function getProfile($username)
     {
+        try {
+            $options  = array('http' => array('user_agent' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)'));
+            $context  = stream_context_create($options);
 
-    }
+            $html = file_get_contents('https://instagram.com/'.$username, false, $context);
+            //Get user ID
+            $subData = substr($html, strpos($html, 'window._sharedData'), strpos($html, '};'));
+            $userID = strstr($subData, '"id":"');
+            $userID = str_replace('"id":"', '', $userID);
+            $userID = strstr($userID, '"', true);
 
-    public function getLikes()
-    {
+            //Download user info
+            $jsonData = file_get_contents('https://i.instagram.com/api/v1/users/'.$userID.'/info/', false, $context);
+            $decodedInfo = json_decode($jsonData);
+            $username = $decodedInfo->user->username;
+            $profilePic = $decodedInfo->user->hd_profile_pic_url_info->url;
+            $follower = $decodedInfo->user->follower_count;
+            $following = $decodedInfo->user->following_count;
+            $full_name = $decodedInfo->user->full_name;
+            $isPrivate = $decodedInfo->user->is_private;
+            $isVerified = $decodedInfo->user->is_verified;
+            $bio = $decodedInfo->user->biography;
+            
+            return [
 
-    }
-
-    public function getRelations()
-    {
-        $this->getFollows();
-    }
-
-    protected function getFollows()
-    {
-
-    }
-
-    protected function getComments()
-    {
+                'full_name' => $full_name,
+                'bio' => $bio,
+                'follower' => $follower,
+                'following' => $following,
+                'profilePic' => $profilePic,
+                'isPrivate' => $isPrivate,
+                'isVerified' => $isVerified,
+            ];
+            
+        } catch (\Exception $e) {
+            return false;
+        }
 
     }
 
