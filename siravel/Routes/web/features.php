117f4307bca5bd403aaf2dbbfefed6fcb6d3d688
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-05-31 21:41:41 -0300

    Traits e components funcionando corretamente no support

--- siravel/Routes/web/features.php
+++ siravel/Routes/web/features.php
@@ -2,15 +2,5 @@
 
 Route::group(
     ['namespace' => 'Features'], function () {
-        Route::group(
-            ['namespace' => 'Manipule'], function () {
-
-
-                Route::get('/creator', 'CreatorController@download')->name('creator');
-                Route::get('/excel', 'ExcelController@download')->name('excell');
-
-        
-            }
-        );
     }
 );
--- siravel/Routes/web/recursos.php
+++ siravel/Routes/web/recursos.php
@@ -14,40 +14,5 @@ Route::group(
             }
         );
 
-
-
-
-        Route::group(
-            ['namespace' => 'Components'], function () {
-                Route::group(
-                    ['as' => 'components.'], function () {
-                        Route::group(
-                            ['namespace' => 'Actors'], function () {
-                                Route::group(
-                                    ['prefix' => 'actors'], function () {
-                                        Route::group(
-                                            ['as' => 'actors.'], function () {
-                                                Route::get('/', 'ProfileController@index')->name('profile');
-                                                Route::get('/show', 'ProfileController@show')->name('profile.show');
-                        
-                                                Route::get('/notifications/unread', 'NotificationsController@unread')->name('notifications.unread');
-                                                Route::get('/notifications', 'NotificationsController@index')->name('notifications.index');
-                                                Route::get('/notifications/count', 'NotificationsController@count')->name('notifications.count');
-
-                                                Route::get('/messages', 'MessagesController@index')->name('messages.index');
-                                                Route::get('/messages/to/{id}', 'MessagesController@create')->name('messages.create');
-                                                Route::post('/messages', 'MessagesController@store')->name('messages.store');
-                                                Route::get('/messages/{id}', 'MessagesController@show')->name('messages.show');
-                                                Route::put('/messages/{id}', 'MessagesController@update')->name('messages.update');
-                                            }
-                                        );
-                                    }
-                                );
-                            }
-                        );
-                    }
-                );
-            }
-        );
     }
 );
