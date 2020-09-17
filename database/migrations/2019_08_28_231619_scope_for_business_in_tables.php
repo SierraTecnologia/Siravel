79a3b16627605d38b1b814fc07e9a646b8f50fbd
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-09-17 03:49:34 -0300

    fix laravel8 compatibility

--- database/migrations/2019_08_28_231619_scope_for_business_in_tables.php
+++ database/migrations/2019_08_28_231619_scope_for_business_in_tables.php
@@ -5,97 +5,95 @@ use Illuminate\Database\Schema\Blueprint;
 class ScopeForBusinessInTables extends Migration
 {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
         if (Schema::hasTable('analytics')) {
-			Schema::table('analytics', function (Blueprint $table) {
-				$table->string('business_code');
-				$table->foreign('business_code')->references('code')->on('businesses');
-			});
-		}
+            Schema::table('analytics', function (Blueprint $table) {
+                $table->string('business_code');
+                $table->foreign('business_code')->references('code')->on('businesses');
+            });
+        }
         if (Schema::hasTable('blogs')) {
-			Schema::table('blogs', function (Blueprint $table) {
-				$table->string('business_code');
-				$table->foreign('business_code')->references('code')->on('businesses');
-			});
-		}
+            Schema::table('blogs', function (Blueprint $table) {
+                $table->string('business_code');
+                $table->foreign('business_code')->references('code')->on('businesses');
+            });
+        }
         if (Schema::hasTable('posts')) {
-			Schema::table('posts', function (Blueprint $table) {
-				$table->string('business_code');
-				$table->foreign('business_code')->references('code')->on('businesses');
-			});
-		}
+            Schema::table('posts', function (Blueprint $table) {
+                $table->string('business_code');
+                $table->foreign('business_code')->references('code')->on('businesses');
+            });
+        }
         if (Schema::hasTable('articles')) {
-			Schema::table('articles', function (Blueprint $table) {
-				$table->string('business_code');
-				$table->foreign('business_code')->references('code')->on('businesses');
-			});
-		}
+            Schema::table('articles', function (Blueprint $table) {
+                $table->string('business_code');
+                $table->foreign('business_code')->references('code')->on('businesses');
+            });
+        }
         if (Schema::hasTable('pages')) {
-			Schema::table('pages', function (Blueprint $table) {
-				$table->string('business_code');
-				$table->foreign('business_code')->references('code')->on('businesses');
-			});
-		}
+            Schema::table('pages', function (Blueprint $table) {
+                $table->string('business_code');
+                $table->foreign('business_code')->references('code')->on('businesses');
+            });
+        }
         if (Schema::hasTable('menus')) {
-			Schema::table('menus', function (Blueprint $table) {
-				$table->string('business_code');
-				$table->foreign('business_code')->references('code')->on('businesses');
-			});
-		}
+            Schema::table('menus', function (Blueprint $table) {
+                $table->string('business_code');
+                $table->foreign('business_code')->references('code')->on('businesses');
+            });
+        }
         if (Schema::hasTable('promotions')) {
-			Schema::table('promotions', function (Blueprint $table) {
-				$table->string('business_code');
-				$table->foreign('business_code')->references('code')->on('businesses');
-			});
-		}
+            Schema::table('promotions', function (Blueprint $table) {
+                $table->string('business_code');
+                $table->foreign('business_code')->references('code')->on('businesses');
+            });
+        }
         if (Schema::hasTable('widgets')) {
-			Schema::table('widgets', function (Blueprint $table) {
-				$table->string('business_code');
-				$table->foreign('business_code')->references('code')->on('businesses');
-			});
-		}
+            Schema::table('widgets', function (Blueprint $table) {
+                $table->string('business_code');
+                $table->foreign('business_code')->references('code')->on('businesses');
+            });
+        }
         if (Schema::hasTable('cards')) {
-			Schema::table('cards', function (Blueprint $table) {
-				$table->string('business_code');
-				$table->foreign('business_code')->references('code')->on('businesses');
-			});
-		}
+            Schema::table('cards', function (Blueprint $table) {
+                $table->string('business_code');
+                $table->foreign('business_code')->references('code')->on('businesses');
+            });
+        }
         if (Schema::hasTable('user_meta')) {
-			Schema::table('user_meta', function (Blueprint $table) {
-				$table->string('business_code');
-				$table->foreign('business_code')->references('code')->on('businesses');
-			});
-		}
+            Schema::table('user_meta', function (Blueprint $table) {
+                $table->string('business_code');
+                $table->foreign('business_code')->references('code')->on('businesses');
+            });
+        }
         if (Schema::hasTable('settings')) {
-			Schema::table('settings', function (Blueprint $table) {
-				$table->string('business_code');
-				$table->foreign('business_code')->references('code')->on('businesses');
+            Schema::table('settings', function (Blueprint $table) {
+                $table->string('business_code');
+                $table->foreign('business_code')->references('code')->on('businesses');
 
-				$table->unique(['code', 'business_code']);
-			});
-		}
+                $table->unique(['setting_key', 'business_code']);
+            });
+        }
         if (Schema::hasTable('followables')) {
-			Schema::table('followables', function (Blueprint $table) {
-				$table->string('business_code');
-				$table->foreign('business_code')->references('code')->on('businesses');
-			});
-		}
-	}
+            Schema::table('followables', function (Blueprint $table) {
+                $table->string('business_code');
+                $table->foreign('business_code')->references('code')->on('businesses');
+            });
+        }
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-        
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
 }
