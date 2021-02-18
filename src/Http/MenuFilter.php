bcd391b1d535a7a3bb2824e8b41fef1fc1cb4394
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2021-02-18 05:17:51 -0300

    CAMN-10

--- src/Http/MenuFilter.php
+++ src/Http/MenuFilter.php
@@ -32,6 +32,6 @@ class MenuFilter extends MenuFilterBase
             return true;
         }
 
-        return \Feature::isActive($feature);
+        return \Features::isActive($feature);
     }
 }
